e7c78443c098fa5af5bf3de09ac22b52da8f460a
Hanro <hlareman2@gmail.com>
2021-11-24 13:29:52 +0200

    removed old file

--- old/modules/internal/install.js
+++
@@ -1,4 +0,0 @@
-console.log("[GMLL]: Generating files...")
-import { getChronicle } from "../handler.js";
-import { getLatest } from "../config";
-await getChronicle(getLatest().release).setup();
--- old/test.js
+++
@@ -1,5 +0,0 @@
-import * as GMLL from "./index.js";
-
-GMLL.setConfig({})
-const gm = await GMLL.init();
-gm.instance.make({"version":"1.0"}).launch({})
