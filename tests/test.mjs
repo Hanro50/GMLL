6577ea54148a166d15a218b3d74faf9fb42e85de
Hanro <hlareman2@gmail.com>
2022-07-31 12:11:14 +0200

    updated the example to use msmc version 4

--- tests/test.mjs
+++ tests/test.mjs
@@ -4,16 +4,16 @@ import { setRoot } from "gmll/config";
 setRoot(".MC")
 await init();
 
-import { fastLaunch } from "msmc";
+import {auth } from "msmc";
 import { getLauncherVersion } from "gmll/config";
-
+const aobj = new auth("select_account");
 const i = new instance({version:"fabric-loader-0.14.5-1.19"});
 i.setIcon("icon_32x32.png", "icon_16x16.png");
 
 //await installForge();
 
 //runtime("minecraft-java-exe")
-const token = wrapper.msmc2token(await fastLaunch("raw", console.log));
+const token = (await (await aobj.launch("raw")).getMinecraft()).gmll();
 console.log(getLauncherVersion())
 
 
