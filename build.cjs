936747d94c022c49a9f8071925d4931797e9fca9
Hanro <hlareman2@gmail.com>
2022-09-25 12:14:30 +0200

    build script fix

--- build.cjs
+++ build.cjs
@@ -1,5 +1,6 @@
 const { execSync } = require("child_process")
-const { rmSync } = require("fs")
+const { rmSync,copyFileSync } = require("fs");
+const { join } = require("path");
 
 console.log("[build]: Removing old files!");
 rmSync("dist", { force: true, recursive: true });
@@ -7,4 +8,6 @@ console.log("[build]: Resolving dependencies!");
 console.log(execSync("npm i").toString());
 console.log("[build]: Building GMLL!");
 console.log(execSync("tsc").toString());
+console.log("[build]: Copying types file");
+copyFileSync(join("src","types.d.ts"),join("types","types.d.ts"))
 console.log("[build]: Done!");
