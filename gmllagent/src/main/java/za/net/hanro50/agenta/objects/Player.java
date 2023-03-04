7d3ac51e6f76a0fc34d61b10bc085e597687905f
Hanro <hlareman2@gmail.com>
2023-03-04 21:39:06 +0200

    Moved
    Please see https://github.com/Hanro50/Agenta/

--- gmllagent/src/main/java/za/net/hanro50/agenta/objects/Player.java
+++
@@ -1,7 +0,0 @@
-package za.net.hanro50.agenta.objects;
-
-public class Player {
-  public String id;
-  
-  public String name;
-}
--- gmllagent/src/main/java/za/net/hanro50/agenta/objects/Profile.java
+++
@@ -1,5 +0,0 @@
-package za.net.hanro50.agenta.objects;
-
-public class Profile extends Player {
-  public Textures[] properties;
-}
